8ca3f3bf8dfd58ff7b8919cd6d139958e58b0f77
Turgut <songulturgut0641@gmail.com>
2022-04-23 18:13:13 +0300

    ödev7

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+"""
+Print numbers from 1 to 100 inclusively following these instructions:
+1. if a number is multiple of 3, print "Fizz" instead of this number,
+2. if a number is multiple of 5, print "Buzz" instead of this number,
+3. for numbers that are multiples of both 3 and 5, print "FizzBuzz",
+4. print the rest of the numbers unchanged.
+"""
+
+for i in range(1,101):
+    if not i%15 :
+        print("FizzBuzz")
+    elif not i%3 :
+        print("Fizz")
+    elif not i%5 :
+        print("Buzz")
+    else:
+        print(i)
